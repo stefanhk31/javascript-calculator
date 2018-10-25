2feb5191b77e2e498920a69b82f47c0722283838
Stefan <shodgeskluck@gmail.com>
2018-10-25 09:46:21 -0400

    Updates

---
+++ precache-manifest.1f11afe760cf56cc3323eb2415084cd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bc7773250f7c400c052",
+    "url": "/javascript-calculator/static/js/runtime~main.9bc77732.js"
+  },
+  {
+    "revision": "3e0329eb6479330f4bea",
+    "url": "/javascript-calculator/static/js/main.3e0329eb.chunk.js"
+  },
+  {
+    "revision": "5cf8e3aa5c0aa1e1ff8b",
+    "url": "/javascript-calculator/static/js/1.5cf8e3aa.chunk.js"
+  },
+  {
+    "revision": "3e0329eb6479330f4bea",
+    "url": "/javascript-calculator/static/css/main.c38d0a0e.chunk.css"
+  },
+  {
+    "revision": "40bb4a6eb7d77ca00aa14a203f4aa326",
+    "url": "/javascript-calculator/index.html"
+  }
+];
